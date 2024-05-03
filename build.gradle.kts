96c81f9977de5c49065a9f16b19ecad5ecb1d760
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2024-05-04 00:32:45 +0300

    add checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
 plugins {
     java
     application
+    checkstyle
 }
-
-group = "hexlet.code"
-version = "1.0-SNAPSHOT"
 
 application {
     mainClassName = "hexlet.code.App"
@@ -14,8 +15,13 @@ repositories {
     mavenCentral()
 }
 
+checkstyle {
+    toolVersion = "10.12.4"
+}
+
 dependencies {
     testCompile("junit", "junit", "4.12")
+    checkstyle("com.puppycrawl.tools", "checkstyle","10.12.4")
 }
 
 configure<JavaPluginConvention> {
