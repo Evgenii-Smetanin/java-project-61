ad70c3536bf8d9743a3b269da35b0dd32e56bdb6
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2024-05-04 22:55:40 +0300

    refactor to comply with checkstyle - 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,9 @@
 package hexlet.code;
 
 public class App {
+    private static final int OPTIONS_NUMBER = 6;
+    private static final int GREET = 1;
+    private static final int EXIT = 0;
     private static final int EVEN = 2;
     private static final int CALCULATOR = 3;
     private static final int GCD = 4;
@@ -9,18 +12,18 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.\n"
-                + "1 - Greet\n"
-                + "2 - Even\n"
-                + "3 - Calculator\n"
-                + "4 - GCD\n"
-                + "5 - Progression\n"
-                + "6 - Prime\n"
-                + "0 - Exit");
+                + GREET + " - Greet\n"
+                + EVEN + " - Even\n"
+                + CALCULATOR + " - Calculator\n"
+                + GCD + " - GCD\n"
+                + PROGRESSION + " - Progression\n"
+                + PRIME + " - Prime\n"
+                + EXIT + " - Exit");
 
         int gameNumber = Cli.getInt();
         System.out.println("Your choice: " + gameNumber);
 
-        if (gameNumber == 0 || gameNumber > 6) {
+        if (gameNumber == EXIT || gameNumber > OPTIONS_NUMBER) {
             return;
         }
 
