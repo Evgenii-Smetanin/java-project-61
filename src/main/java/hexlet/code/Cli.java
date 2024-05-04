01192acc9e50957d579ca3c7b08d03ef9d5de4df
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2024-05-04 12:52:04 +0300

    reformat tabs to spaces

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-	public static String getInput() {
-		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
-		sc.close();
-		return input;
-	}
+    public static String getInput() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
+        return input;
+    }
 }
