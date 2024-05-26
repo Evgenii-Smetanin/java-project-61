2c06db62cde27ecbba186eee8bfe14c94c3edb28
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2024-05-26 14:14:06 +0300

    refactor Calculator

--- app/src/main/java/hexlet/code/game/Calculator.java
+++ app/src/main/java/hexlet/code/game/Calculator.java
@@ -8,35 +8,16 @@ import static hexlet.code.Engine.ROUNDS;
 public class Calculator {
     private static final String TASK = "What is the result of the expression?";
     private static final int CALCULATOR_UPPER_BOUND = 25;
-    private static final int OPERATIONS = 3;
-
-    private static final int SUM = 1;
-    private static final int SUBSTRACT = 2;
-    private static final int MULTIPLY = 3;
+    private static final int OPERATIONS_MAX_INDEX = 2;
 
     public static void run() {
         String[][] questionsAnswers = new String[ROUNDS][2];
 
         for (int i = 0; i < questionsAnswers.length; i++) {
+            final char[] operators = {'+', '-', '*'};
             int firstOperand = RandomUtils.generateNumber(1, CALCULATOR_UPPER_BOUND);
             int secondOperand = RandomUtils.generateNumber(1, CALCULATOR_UPPER_BOUND);
-            int operationNum = RandomUtils.generateNumber(1, OPERATIONS);
-            String operation;
-
-            switch (operationNum) {
-                case SUM:
-                    operation = "+";
-                    break;
-                case SUBSTRACT:
-                    operation = "-";
-                    break;
-                case MULTIPLY:
-                    operation = "*";
-                    break;
-                default:
-                    throw new IllegalArgumentException("Illegal operation number: '" + operationNum
-                            + "', Operation must be between 0-2 inclusive");
-            }
+            char operation = operators[RandomUtils.generateNumber(0, OPERATIONS_MAX_INDEX)];
 
             questionsAnswers[i][0] = firstOperand + " " + operation + " " + secondOperand;
             questionsAnswers[i][1] = String.valueOf(calculateExpressionResult(firstOperand, secondOperand, operation));
@@ -45,13 +26,13 @@ public class Calculator {
         Engine.run(TASK, questionsAnswers);
     }
 
-    private static int calculateExpressionResult(int a, int b, String operation) {
+    private static int calculateExpressionResult(int a, int b, char operation) {
         switch (operation) {
-            case "+":
+            case '+':
                 return a + b;
-            case "-":
+            case '-':
                 return a - b;
-            case "*":
+            case '*':
                 return a * b;
             default:
                 throw new IllegalArgumentException("Illegal operation: '" + operation
